d5d93d68ac6bacbc345341569a300a60fed2d4d9
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-08-17 11:21:18 +0530

    import java.util.*; public class Cmind{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int p1 = sc.nextInt();         int p2 = sc.nextInt();         int p3 = sc.nextInt();         int p4 = sc.nextInt();         if(p1>=10 && p2>=10 && p3>=10 && p4>=10 )         System.out.print("YES");         else         System.out.print("NO");     } }

---
+++ Target.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Cmind{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int p1 = sc.nextInt();
+        int p2 = sc.nextInt();
+        int p3 = sc.nextInt();
+        int p4 = sc.nextInt();
+        if(p1>=10 && p2>=10 && p3>=10 && p4>=10 )
+        System.out.print("YES");
+        else
+        System.out.print("NO");
+    }
+}
