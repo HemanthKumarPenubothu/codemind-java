a2ce165d20eb35ce2e4021891921a88b2b0c89ed
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-08-19 12:15:26 +0530

    import java.util.*; public class Codemind{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         int z = sc.nextInt();         int C=x*y*z*2*512;         System.out.print(C);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Codemind{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        int C=x*y*z*2*512;
+        System.out.print(C);
+        
+    }
+}
