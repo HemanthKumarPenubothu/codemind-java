08ea60ee9eb8d2b5492e9517755bf66834a4359b
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-08-26 11:00:38 +0530

    import java.util.*; public class rohit{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         long n=sc.nextInt();         for(long i=1;i<=n;i++){             long x,y,c=0;             x=sc.nextInt();             y=sc.nextInt();
                for(long j=0;j<=1000;j++){
                    if((y)%(x)==0){
                        System.out.println("YES");
                        c++;
                        break;
                    }
                
                else{
                    long w=y%x;
                    y=y+w;
                    x=x+w;
                }
                
            }
                if(c==0){
                    System.out.println("NO");
                }
         }
    }
    }

---
+++ Can_be_a_factor?.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class rohit{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextInt();
+        for(long i=1;i<=n;i++){
+            long x,y,c=0;
+            x=sc.nextInt();
+            y=sc.nextInt();
+            
+            for(long j=0;j<=1000;j++){
+                if((y)%(x)==0){
+                    System.out.println("YES");
+                    c++;
+                    break;
+                }
+            
+            else{
+                long w=y%x;
+                y=y+w;
+                x=x+w;
+            }
+            
+        }
+            if(c==0){
+                System.out.println("NO");
+            }
+     }
+}
+}
